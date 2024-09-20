72e1320f9ea518bb81e4acca0a87249c6eb72476
lucasmercado101 <lucasmercado101@gmail.com>
2024-09-20 18:12:30 -0300

    trpc client working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createSignal } from "solid-js";
+import solidLogo from "./assets/solid.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import { trpc } from "./trpc";
 
 function App() {
-  const [count, setCount] = createSignal(0)
+  const [count, setCount] = createSignal(0);
+  async function get() {
+    const res = await trpc.userList.query();
+    console.log(res);
+  }
 
   return (
     <>
@@ -18,9 +24,7 @@ function App() {
       </div>
       <h1>Vite + Solid</h1>
       <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
+        <button onClick={() => get()}>get again</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and Solid logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
