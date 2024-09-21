5c7f9ee6ad948a242b1d586e1eafb23c2241fede
lucasmercado101 <lucasmercado101@gmail.com>
2024-09-21 19:56:53 -0300

    doing styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { createSignal } from "solid-js";
-import solidLogo from "./assets/solid.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
+import First from "./first";
 import { trpc } from "./trpc";
+import "./main.scss";
 
 function App() {
   const [count, setCount] = createSignal(0);
@@ -12,28 +11,7 @@ function App() {
     console.log(res);
   }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => get()}>get again</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
-  );
+  return <First />;
 }
 
 export default App;
---
+++ src/first.tsx
@@ -0,0 +1,42 @@
+export default function First() {
+  return (
+    <div class="main-body">
+      <img src="/src/assets/logo.svg" class="bg-logo" alt="logo" />
+      <div class="students-list">
+        <p class="header">name</p>
+        <ul>
+          <li>
+            <div>
+              <span class="s-badge m">M</span>
+              <p>Miller</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <span class="s-badge m">M</span>
+              <p>Eagle</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <span class="s-badge m">M</span>
+              <p>Cabbage</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <span class="s-badge">F</span>
+              <p>Peccary</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <span class="s-badge">F</span>
+              <p>Ostrich</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
